8e0d65db62fa48733af464ffbec727d6e66ed6ad
Jared Wyce <wycejared@gmail.com>
2022-10-16 20:00:13 -0400

    🏗 update prisma schema and ogs experiement

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,15 +1,34 @@
-import { t } from "../trpc";
-import { z } from "zod";
+import { t } from '../trpc';
+import { z } from 'zod';
+import ogs from 'open-graph-scraper';
 
 export const exampleRouter = t.router({
-  hello: t.procedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    }),
-  getAll: t.procedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+	hello: t.procedure
+		.input(z.object({ text: z.string().nullish() }).nullish())
+		.query(({ input }) => {
+			return {
+				greeting: `Hello ${input?.text ?? 'world'}`,
+			};
+		}),
+	og: t.procedure.input(z.string().url()).query(({ input }) => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const ogPromise = new Promise((resolve, reject) => {
+			ogs(
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				{ url: input, onlyGetOpenGraphInfo: true, downloadLimit: 100_000_000 },
+				(err, res) => {
+					if (!err) {
+						resolve(res);
+					}
+					reject(err);
+				}
+			);
+		});
+
+		return ogPromise;
+	}),
+	getAll: t.procedure.query(({ ctx }) => {
+		return ctx.prisma.example.findMany();
+	}),
 });
